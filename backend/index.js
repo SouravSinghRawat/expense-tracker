5259ef0302dc4d1f447c338dbd5d046ee234fa49
SouravSinghRawat <souravprofessional20122001@gmail.com>
2024-10-15 23:45:31 +0530

    changed

--- backend/index.js
+++ backend/index.js
@@ -6,14 +6,17 @@ const AuthRouter = require('./Routes/AuthRouter');
 const ProductRouter = require('./Routes/ProductRouter');
 const ExpenseRouter = require('./Routes/ExpenseRouter');
 const ensureAuthenticated = require('./Middlewares/Auth');
+const path=require('path');
 
 require('dotenv').config();
 require('./Models/db');
 const PORT = process.env.PORT || 8080;
 
-app.get('/ping', (req, res) => {
-    res.send('PONG');
-});
+// app.get('/ping', (req, res) => {
+//     res.send('PONG');
+// });
+
+const _dirname=path.resolve();
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -21,6 +24,14 @@ app.use('/auth', AuthRouter);
 app.use('/products', ProductRouter);
 app.use('/expenses', ensureAuthenticated, ExpenseRouter)
 
+
+app.use(express.static(path.join(_dirname,
+    "/frontend/build"
+)));
+
+app.get('*', (req,res)=>{
+    res.sendFile(path.resolve(_dirname,"frontend", "build", "index.html"));
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`)
